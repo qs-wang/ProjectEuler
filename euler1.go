3cae8fea88cd144d80ef8dbe66bb5fe95d1ce615
Q.S. Wang <wangqs_eclipse@yahoo.com>
2022-01-03 16:31:10 +0800

    project euler q 24

--- euler1.go
+++ euler1.go
@@ -1,18 +1,23 @@
 package projecteuler
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// NonAbundantSums https://projecteuler.net/problem=23
 func NonAbundantSums() int {
-	abundantNums := make([]int, 28124)
-	k := 0
+	abundantNums := []int{}
 	for i := 1; i <= 28123; i++ {
 		if GetFactorSum(i) > i {
-			abundantNums[k] = i
-			k++
+			abundantNums = append(abundantNums, i)
 		}
 	}
 
 	set := make([]int, 28124)
-	for i := 0; i < k; i++ {
-		for j := i; j < k; j++ {
+	for i := 0; i < len(abundantNums); i++ {
+		for j := i; j < len(abundantNums); j++ {
 			x := abundantNums[i] + abundantNums[j]
 			if x <= 28123 {
 				set[x] = x
@@ -28,5 +33,56 @@ func NonAbundantSums() int {
 	}
 
 	return sum
+}
 
+// Find1MillionthLexicographicPermutation https://projecteuler.net/problem=24
+func Find1MillionthLexicographicPermutation(nums []int) string {
+	count := 1
+	next := nums
+
+	for {
+		if count == 1000000 {
+			break
+		}
+		next = NextLexicographicPermutations(next)
+		count++
+	}
+
+	return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(next)), ""), "[]")
+}
+
+// NextLexicographicPermutations finds the next lexicographic permutation of the given slice of ints.
+func NextLexicographicPermutations(nums []int) []int {
+	if len(nums) == 1 {
+		return nums
+	}
+
+	if len(nums) == 2 {
+		nums[0], nums[1] = nums[1], nums[0]
+		return nums
+	}
+
+	var k int
+	for i := len(nums) - 1; i >= 0; i-- {
+		if i == 1 && nums[1] < nums[0] {
+			// nums[len(nums)-1], nums[len(nums)-2] = nums[len(nums)-2], nums[len(nums)-1]
+			return nums
+		}
+
+		if nums[i-1] < nums[i] {
+			k = i - 1
+			break
+		}
+	}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] > nums[k] {
+			nums[i], nums[k] = nums[k], nums[i]
+
+			sort.Ints(nums[k+1:])
+			break
+		}
+	}
+
+	return nums
 }
--- euler1_test.go
+++ euler1_test.go
@@ -9,3 +9,31 @@ import (
 func TestNonAbundantSums(t *testing.T) {
 	assert.Equal(t, 4179871, NonAbundantSums())
 }
+
+func TestLexicographicPermutations(t *testing.T) {
+	assert.Equal(t, []int([]int{2, 1}), NextLexicographicPermutations([]int{1, 2}))
+}
+
+func TestLexicographicPermutations1(t *testing.T) {
+	assert.Equal(t, []int([]int{0, 2, 1}), NextLexicographicPermutations([]int{0, 1, 2}))
+}
+
+func TestLexicographicPermutations2(t *testing.T) {
+	assert.Equal(t, []int([]int{0, 1, 2, 3, 4, 5, 6, 7, 9, 8}), NextLexicographicPermutations([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}))
+}
+
+func TestLexicographicPermutations3(t *testing.T) {
+	assert.Equal(t, []int([]int{0, 1, 3, 0, 2, 3, 5}), NextLexicographicPermutations([]int{0, 1, 2, 5, 3, 3, 0}))
+}
+
+func TestLexicographicPermutations4(t *testing.T) {
+	assert.Equal(t, []int([]int{1, 0, 2, 3, 4, 5, 6, 7, 8, 9}), NextLexicographicPermutations([]int{0, 9, 8, 7, 6, 5, 4, 3, 2, 1}))
+}
+
+func TestLexicographicPermutations5(t *testing.T) {
+	assert.Equal(t, []int([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}), NextLexicographicPermutations([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}))
+}
+
+func TestFind1MillionthLexicographicPermutation(t *testing.T) {
+	assert.Equal(t, "2783915460", Find1MillionthLexicographicPermutation([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}))
+}
